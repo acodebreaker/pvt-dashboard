63ad7699941006c5b3f3376582f06086bb116633
ankitrawat <itsankitrawat@gmail.com>
2017-05-26 18:45:34 +0530

    hello world component created

---
+++ dev/app.component.ts
@@ -0,0 +1,20 @@
+import {Component} from 'angular2/core';
+
+
+@Component({
+    selector: 'my-app',
+
+    template: `
+    <h3> hello world</h3>
+    `
+ ,
+ styleUrls :'src/css/app.css'
+
+})
+
+
+export class AppComponent 
+{
+ 
+
+}
