5f6cf81a1b33373ff52050f2f96fe773fef8827e
dheerajdac <dheerajdac@gmail.com>
2017-05-27 20:54:48 +0530

    mvn project structure

---
+++ config/webpack.common.js
@@ -0,0 +1,68 @@
+/**
+ * Created by dheeraj on 27/5/17.
+ */
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./helpers');
+
+module.exports = {
+    entry: {
+        'vendor': './src/main/resources/angular/vendor.ts',
+        'app': './src/main/resources/angular/main.ts'
+    },
+
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                loaders: [
+                    {
+                        loader: 'awesome-typescript-loader',
+                        options: { configFileName: helpers.root('./src/main/resources/angular', 'tsconfig.json') }
+                    } , 'angular2-template-loader'
+                ]
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
+                loader: 'file-loader?name=assets/[name].[hash].[ext]'
+            },
+            {
+                test: /\.css$/,
+                exclude: helpers.root('src', 'app'),
+                loader: ExtractTextPlugin.extract({ fallbackLoader: 'style-loader', loader: 'css-loader?sourceMap' })
+            },
+            {
+                test: /\.css$/,
+                include: helpers.root('src', 'app'),
+                loader: 'raw-loader'
+            }
+        ]
+    },
+
+    plugins: [
+        // Workaround for angular/angular#11580
+        new webpack.ContextReplacementPlugin(
+            // The (\\|\/) piece accounts for path separators in *nix and Windows
+            /angular(\\|\/)core(\\|\/)(esm(\\|\/)src|src)(\\|\/)linker/,
+            helpers.root('./src/main/resources/angular'), // location of your src
+            {} // a map of your routes
+        ),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor']
+        })/*,
+
+        new HtmlWebpackPlugin({
+            template: './src/main/webapp/index.html'
+        })*/
+    ]
+};
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+/**
+ * Created by dheeraj on 27/5/17.
+ */
+
+module.exports = require('./config/webpack.dev.js');
